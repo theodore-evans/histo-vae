f1a8640a5e73d3fee04f75692b366fc7c9fbbd1d
Theodore Evans <theodore.evans@dai-labor.de>
2020-07-28 17:15:17 +0200

    parsing notebook writes data to csv (again)

--- Models/Augment.py
+++ Models/Augment.py
@@ -9,21 +9,7 @@ from PIL import Image
 import os
 
 class Augment:
-
-    def __init__(self, folder_path):
-        
-        if not os.path.isdir(folder_path) : raise Exception(f"{folder_path} is not a valid directory")
-        self.folder_path = folder_path
-        self.export_path = os.path.join(self.folder_path, "augment/")
-        print("Exporting transformed images to " + self.export_path)
-        
-
-    def TransformImage(self, image_filename, image_to_transform: ndarray, transformations = ['rotate, flip, noise']):
-
-        if not os.path.isfile(os.path.join(self.folder_path,image_filename)) : raise Exception(f"{image_filename} is not a valid file")
-        
-        if not os.path.exists(self.export_path):
-            os.mkdir(self.export_path)
+    def TransformImage(self, image_to_transform: ndarray, transformations = ['rotate, flip, noise']):
         
         available_transformations = {
             'rotate' : self.RandomRotation,
@@ -36,37 +22,15 @@ class Augment:
         num_transformations_to_apply = random.randint(0, len(transformations))
         num_transformations = 0
 
-        transformed_image = None
+        transformed_images = []
         
         while num_transformations < num_transformations_to_apply:
             key = random.choice(list(transformations))
             transformed_image = transformations.pop(key)(image_to_transform)
-    
-            new_filename = f"{image_filename.rsplit('.', 1)[0]}_augment_{num_transformations}.png"
-            image_path = os.path.join(self.export_path, new_filename)
-
-            image = Image.fromarray((transformed_image * 255).astype(np.uint8))
-            image.save(image_path, 'png')
-
-            # print(f"Saving transformed image {new_filename}.png with transform {key}")
+            transformed_images.append(transformed_image)
             num_transformations += 1
 
-    def TransformAllImages(self, num_files_to_generate):
-
-        images_to_transform = [ f for f in os.listdir(self.folder_path) if os.path.isfile(os.path.join(self.folder_path, f))]
-        num_generated_files = 0
-
-        while num_generated_files <= num_files_to_generate:
-            image_name = random.choice(images_to_transform)
-            image_path = os.path.join(self.folder_path, image_name)
-
-            image_to_transform = np.asarray(Image.open(image_path))
-
-            self.TransformImage(image_name, image_to_transform)
-
-            images_to_transform.remove(image_name) # don't transform the same image twice
-            num_generated_files += 1
-
+        return transformed_images
 
     def RandomRotation(self, image_array : ndarray):
         # rotates the image by 90, 180 or 270 degrees at random
