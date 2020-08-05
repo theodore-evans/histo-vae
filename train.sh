b4ca660c0356644c62555b1a1a48b80707ef49e6
Emirhan Karagül <emirhan.karaguel@dai-labor.de>
2020-08-05 15:20:12 +0200

    Change training for pcam dataset and add early stopping callback

--- train.sh
+++ train.sh
@@ -1,8 +1,9 @@
 docker build -t vae-test -f Dockerfile.vae .
 
-docker run --rm --gpus=all \
+docker run --rm -d --gpus=all --name=$USER-$(uuidgen) \
 -v $(pwd)/logs:/logs \
 -v $(pwd)/Datasets:/Datasets \
 -v $(pwd)/weights:/weights \
+-v $(pwd)/data:/data \
 vae-test \
-vae_script.py --weights-dir=./weights --logs-dir=./logs
+vae_script.py --weights-dir=./weights --logs-dir=./logs -dim=64
--- vae_script.py
+++ vae_script.py
@@ -1,11 +1,13 @@
 #!/usr/bin/env python3
 # adapted from https://keras.io/examples/generative/vae/
-#%%
+# %%
 import numpy as np
 import tensorflow as tf
 from tensorflow import keras
 from tensorflow.keras import layers, callbacks
 import pandas as pd
+import random
+import h5py
 
 import sys
 import argparse
@@ -19,6 +21,14 @@ parser.add_argument(
 parser.add_argument(
     '--weights-dir', type=Path, default=None,
     help='Debug: Path to save the weights'
+)
+parser.add_argument(
+    '-dim', type=int, default=1024,
+    help='The size of latent dimension representation'
+)
+parser.add_argument(
+    '--batch-size', type=int, default=128,
+    help='Batch Size'
 )
 
 args = parser.parse_args()
@@ -34,7 +44,9 @@ if args.logs_dir:
 
 print('List GPUs:', tf.config.list_physical_devices('GPU'))
 
-#%%
+# %%
+
+
 class Sampling(layers.Layer):
     """Uses (z_mean, z_log_var) to sample z, the vector encoding a digit."""
 
@@ -45,6 +57,7 @@ class Sampling(layers.Layer):
         epsilon = tf.keras.backend.random_normal(shape=(batch, dim))
         return z_mean + tf.exp(0.5 * z_log_var) * epsilon
 
+
 class VAE(keras.Model):
     def __init__(self, encoder, decoder, **kwargs):
         super(VAE, self).__init__(**kwargs)
@@ -53,6 +66,12 @@ class VAE(keras.Model):
 
     def reconstruct(self, data):
         z_mean, z_log_var, z = encoder(data)
+        reconstruction = decoder(z)
+        return reconstruction
+
+    def call(self, data, training=False):
+        if isinstance(data, tuple):
+            data = data[0]
         reconstruction = decoder(z)
         return reconstruction
 
@@ -77,16 +96,25 @@ class VAE(keras.Model):
             "reconstruction_loss": reconstruction_loss,
             "kl_loss": kl_loss,
         }
-#%%
-cp_callback = callbacks.ModelCheckpoint(filepath = str(checkpoint_file),
-                                                 save_weights_only=True)
-#%%
-latent_dim = 1024
+
+
+# %%
+cp_callback = callbacks.ModelCheckpoint(filepath=str(checkpoint_file),
+                                        save_weights_only=True)
+es_callback = callbacks.EarlyStopping(
+    monitor="loss",
+    patience=5,
+)
+# %%
+latent_dim = args.dim
 
 encoder_inputs = keras.Input(shape=(32, 32, 3))
-x = layers.Conv2D(64, kernel_size=4, activation="relu", strides=2, padding="same")(encoder_inputs)
-x = layers.Conv2D(128, kernel_size=4, activation="relu", strides=2, padding="same")(x)
-x = layers.Conv2D(512, kernel_size=4, activation="relu", strides=2, padding="same")(x)
+x = layers.Conv2D(64, kernel_size=4, activation="relu",
+                  strides=2, padding="same")(encoder_inputs)
+x = layers.Conv2D(128, kernel_size=4, activation="relu",
+                  strides=2, padding="same")(x)
+x = layers.Conv2D(512, kernel_size=4, activation="relu",
+                  strides=2, padding="same")(x)
 encoded = layers.Flatten()(x)
 z_mean = layers.Dense(latent_dim, name="z_mean")(encoded)
 z_log_var = layers.Dense(latent_dim, name="z_log_var")(encoded)
@@ -97,21 +125,46 @@ encoder.summary()
 latent_inputs = keras.Input(shape=(latent_dim,))
 x = layers.Dense(2048)(latent_inputs)
 x = layers.Reshape((4, 4, 128))(x)
-x = layers.Conv2DTranspose(256, kernel_size=4, activation="relu", strides=2, padding="same")(x)
-x = layers.Conv2DTranspose(64, kernel_size=4, activation="relu", strides=2, padding="same")(x)
-decoder_outputs = layers.Conv2DTranspose(3, kernel_size=4, activation="relu", strides=2, padding="same")(x)
+x = layers.Conv2DTranspose(
+    256, kernel_size=4, activation="relu", strides=2, padding="same")(x)
+x = layers.Conv2DTranspose(
+    64, kernel_size=4, activation="relu", strides=2, padding="same")(x)
+decoder_outputs = layers.Conv2DTranspose(
+    3, kernel_size=4, activation="relu", strides=2, padding="same")(x)
 decoder = keras.Model(latent_inputs, decoder_outputs, name="decoder")
 decoder.summary()
 
-#%%
+# %%
+TRAIN_X_FILE = 'camelyonpatch_level_2_split_train_x.h5'
+train_x = h5py.File(Path('./data', TRAIN_X_FILE), 'r',
+                    libver='latest', swmr=True)['x']
 
-data = pd.read_pickle('./Datasets/cells_32x32_it_50.pkl')
-X = np.stack(data['features'].to_numpy(),axis=0)   
+
+def data_generator(x, batch_size=None):
+    index = range(len(x))
+
+    while True:
+        index_sample = index
+        if batch_size is not None:
+            index_sample = sorted(random.sample(index, batch_size))
+        x_pos = random.randint(0, 64)
+        y_pos = random.randint(0, 64)
+        x_data = x[index_sample, x_pos:x_pos+32, y_pos:y_pos+32] / 255.0
+        yield x_data
+
+data_size = len(train_x)
+batch_size = args.batch_size
+steps_per_epoch = data_size // batch_size
+
 vae = VAE(encoder, decoder)
 vae.compile(optimizer=keras.optimizers.Adam())
-vae.fit(X, epochs=150, batch_size=128, callbacks=[cp_callback])
+vae.fit(data_generator(train_x, batch_size=batch_size),
+        epochs=100,
+        steps_per_epoch = steps_per_epoch,
+        batch_size=batch_size,
+        callbacks=[cp_callback, es_callback])
 
-#%%
+# %%
 
 if args.logs_dir:
     sys.stdout.close()
